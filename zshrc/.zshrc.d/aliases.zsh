d31591d571ce724f466e59e7199ab39eb7bbbe44
Marius Brauterfallet <84996611+marius-brauterfallet@users.noreply.github.com>
2026-02-15 22:08:58 +0000

    add duh alias

--- zshrc/.zshrc.d/aliases.zsh
+++ zshrc/.zshrc.d/aliases.zsh
@@ -3,3 +3,11 @@ alias ~="cd ~"
 
 alias ll="ls -lh"
 alias la="ls -lha"
+
+alias mosh="mosh -p 60050:60100"
+
+alias trem="transmission-remote"
+alias tra="transmission-remote -a"
+alias trl="transmission-remote -l"
+
+alias duh="du -h -d 1"
